b3076ca0c23933c5ba316a89766e3dda24c823ed
David <drizzzydave@gmail.com>
2021-09-22 08:42:47 +0100

    Home page build

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,17 @@
+//import React from 'react'
+
+const Home = () => {
+  return (
+    <section className="text-center pt-10 md:pt-20 flex-grow">
+      <h1 className=" text-2xl md:text-5xl font-bold leading-snug tracking-wider">
+        PoorBoy Stocks Trends
+      </h1>
+      <p className="max-w-lg mx-auto pt-6 text-base md:text-lg leading-8">
+        Welcome to PoorBoy Stocks Trends, your one stop shop for daily updates
+        on Stocks, Forex, Crypto, Futures, Commodities and Indices.
+      </p>
+    </section>
+  );
+};
+
+export default Home;
