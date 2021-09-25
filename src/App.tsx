d2f31e3d3ca7532528d66bde9d30bb683d27c437
David <drizzzydave@gmail.com>
2021-09-25 16:40:53 +0100

    routes for Stocks and Login pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,30 @@
 import { FC } from "react";
-import { BrowserRouter as Router /* Route, Switch  */ } from "react-router-dom";
-import { Navbar } from "./components/Navbar/Navbar";
+import {
+  BrowserRouter as Router,
+  Route /* Route, Switch  */,
+  Switch,
+} from "react-router-dom";
+import { Footer, Navbar } from "./components";
+import MainLayOut from "./components/MainAppLayout";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Stocks from "./pages/Stocks";
 
 //import "./App.css";
 
 const App: FC = () => {
   return (
     <Router>
-      <div className="bg-blue px-20 text-white bg-blue-900 h-screen overflow-hidden">
+      <div className="bg-blue  text-white bg-blue-900 flex flex-col h-full overflow-hidden min-h-screen">
         <Navbar />
+        <MainLayOut>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/stocks" component={Stocks} />
+            <Route exact path="/login" component={Login} />
+          </Switch>
+        </MainLayOut>
+        <Footer />
       </div>
     </Router>
   );
