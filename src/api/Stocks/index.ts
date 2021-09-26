42aed766fb0c5357604ecf4c204c2fa15045aa54
David <drizzzydave@gmail.com>
2021-09-26 10:43:51 +0100

    set up api for getting Stocks info

---
+++ src/api/Stocks/index.ts
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+export const baseUrl = "https://cloud.iexapis.com/stable";
+const twelveDataUrl = "https://api.twelvedata.com";
+
+export const getStockInfo = async (symbol: string) => {
+  try {
+    const res = await axios.get(`${twelveDataUrl}/stocks?symbol=${symbol}`);
+    return res.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
