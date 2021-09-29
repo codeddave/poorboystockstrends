798f553a4292a5764f41b25fe7e212f9eb132cfa
David <drizzzydave@gmail.com>
2021-09-29 09:54:04 +0100

    created forex page

---
+++ src/pages/Forex/index.tsx
@@ -0,0 +1,67 @@
+//import { useState } from "react"
+import { FC } from "react";
+import { getForexInfo } from "../../api";
+import Loader from "react-loader-spinner";
+import { useSearch } from "../../components/hooks";
+
+const Forex: FC = () => {
+  const {
+    fetchedData: forexData,
+    isLoading,
+    selectedItem: selectedForexPair,
+    searchQuery,
+    handleChange,
+    handleSelectedItem: handleSelectedForexPair,
+  } = useSearch(getForexInfo);
+
+  console.log(selectedForexPair);
+  console.log(forexData);
+  return (
+    <div className="flex-grow">
+      <p className="text-center text-xl">Search</p>
+      <div className=" w-full lg:w-2/3 mx-auto bg-white rounded flex items-center pr-2 mt-2">
+        <input
+          className="w-full  mx-auto text-black py-1.5 pl-2 rounded outline-none "
+          placeholder="Search..."
+          onChange={handleChange}
+          value={searchQuery}
+        />
+
+        {isLoading ? (
+          <Loader type="TailSpin" color="#000000" height={20} width={30} />
+        ) : null}
+      </div>
+      {searchQuery ? (
+        <ul className="mt-2 w-full lg:w-2/3 mx-auto bg-white  divide-y rounded">
+          {forexData?.data?.map((forex: any) => (
+            <li
+              key={forex.currency_base}
+              onClick={() => handleSelectedForexPair(forex.symbol)}
+              className=" px-3 text-xs md:text-lg text-black py-1 hover:bg-blue-600 hover:text-gray-200  flex"
+            >
+              <span className=" text-sm lg:text-lg ">
+                {forex.currency_base}
+              </span>
+              {/* ,
+              <span className=" text-sm lg:text-lg ">
+                &nbsp; {stock.country}
+              </span>
+              ,
+              <span className=" text-sm lg:text-lg ">
+                &nbsp; {stock.exchange}
+              </span> */}
+            </li>
+          ))}
+        </ul>
+      ) : null}
+
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod vitae
+        laborum laudantium saepe ab, atque quos, rerum tempora autem porro
+        voluptate iure quae eum. Totam minus sed consequuntur autem pariatur.
+      </p>
+    </div>
+  );
+};
+
+export default Forex;
