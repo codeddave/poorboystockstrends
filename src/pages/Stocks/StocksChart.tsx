1d485b7deb1e67798beada3aa308f09000ca5821
David <drizzzydave@gmail.com>
2021-10-02 14:41:54 +0100

    StocksChart component

---
+++ src/pages/Stocks/StocksChart.tsx
@@ -0,0 +1,13 @@
+import CanvasJSReact from "../../canvasjs.stock.react";
+
+const CanvasJsStockChart = CanvasJSReact.CanvasJSStockChart;
+
+const StocksChart = () => {
+  return (
+    <div>
+      <CanvasJsStockChart />
+    </div>
+  );
+};
+
+export default StocksChart;
