c0fe2341cf2015b692b68f17c72e0696cde4743b
David <drizzzydave@gmail.com>
2021-10-04 08:58:16 +0100

    built register screen

---
+++ src/pages/Register/index.tsx
@@ -0,0 +1,46 @@
+import { Field, Form, Formik } from "formik";
+import CustomFormInput from "../../components/Form/CustomFormInput";
+import { HiOutlineMail, HiOutlineUser } from "react-icons/hi";
+import { VscLock } from "react-icons/vsc";
+const initialValues = {
+  username: "",
+  email: " ",
+  password: "",
+};
+const Register = () => {
+  return (
+    <div className="mx-auto w-3/4">
+      <Formik
+        initialValues={initialValues}
+        onSubmit={() => console.log("bhdvhjveEJEVVVJBe")}
+      >
+        {() => (
+          <Form>
+            <Field
+              component={CustomFormInput}
+              name="username"
+              type="text"
+              title="Username"
+              icon={HiOutlineUser}
+            />
+            <Field
+              component={CustomFormInput}
+              name="email"
+              type="email"
+              title="Email"
+              icon={HiOutlineMail}
+            />
+            <Field
+              component={CustomFormInput}
+              name="password"
+              title="Password"
+              icon={VscLock}
+            />
+          </Form>
+        )}
+      </Formik>
+    </div>
+  );
+};
+
+export default Register;
