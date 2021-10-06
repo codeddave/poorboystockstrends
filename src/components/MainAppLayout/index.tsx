2d7ace2f49db22674642515199c4a9047dd2aded
David <drizzzydave@gmail.com>
2021-10-07 00:37:57 +0100

    updated mainLayout styling

--- src/components/MainAppLayout/index.tsx
+++ src/components/MainAppLayout/index.tsx
@@ -4,21 +4,15 @@ import { FC } from "react";
 //max-h-screen overflow-y-scroll
 const MainLayOut: FC = ({ children }) => {
   return (
-    <section className="grid md:grid-cols-4 flex-grow t-10 md:pt-20 ">
+    <section className="grid md:grid-cols-4 flex-grow t-10 md:pt-32">
       <div className=" hidden md:flex flex-col items-center px-2  text-center  ">
-        <p>tevihvksdv</p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis hic,
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis hic,
-        </p>
+        <p className="text-3xl">Ads go here</p>
       </div>
 
-      <div className=" md:col-span-2">{children}</div>
+      <div className="px-4 md:px-0 pt-24 md:pt-0 md:col-span-2">{children}</div>
 
       <div className="hidden md:flex flex-col items-center px-4">
-        <p>jekbvJEBvjbdjbv</p>
+        <p className="text-3xl">Ads go here</p>
       </div>
     </section>
   );
