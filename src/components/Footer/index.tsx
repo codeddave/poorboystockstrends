35eba3ea813ef7471b1a0e9781d0b8afec4cc05e
David <drizzzydave@gmail.com>
2021-10-07 00:15:09 +0100

    updated footer styling

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -2,7 +2,7 @@ export const Footer = () => {
   return (
     <footer>
       <ul className="flex justify-center pb-8 ">
-        <li className="pl-10">About</li>
+        <li>About</li>
         <li className="pl-10">Advertise</li>
         <li className="pl-10">Privacy</li>
         <li className="pl-10">Contact</li>
