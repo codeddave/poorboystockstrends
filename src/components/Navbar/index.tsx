3495f048dad69cf53440411f19ac33b4471dbb3b
David <drizzzydave@gmail.com>
2021-10-06 08:03:34 +0100

    switched implementation of the navbar, used grid

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -4,39 +4,40 @@ import "./Navbar.scss";
 //create navItem component
 export const Navbar = () => {
   return (
-    <nav className="pt-8 w-full lg:w-11/12 mx-auto">
-      <ul className=" nav-ul flex justify-between lg:pr-14 full">
-        <div>
-          <NavLink
-            to="/"
-            className="ml-2 md:ml-6 font-bold text-xl cursor-pointer"
-          >
-            POORBOY STOCKS TRENDS
-          </NavLink>
-        </div>
-        <div>
-          <NavLink
-            to="/stocks"
-            className=" cursor-pointer "
-            activeClassName="nav-active"
-          >
-            Stocks
-          </NavLink>
-          <NavLink
-            to="/forex"
-            className="ml-2 md:ml-6 cursor-pointer "
-            activeClassName="nav-active"
-          >
-            Forex
-          </NavLink>
-          <NavLink
-            to="/crypto"
-            className="ml-2 md:ml-6 cursor-pointer"
-            activeClassName="nav-active"
-          >
-            Crypto
-          </NavLink>
-          {/*      <NavLink
+    <>
+      <nav className="pt-8 w-full  mx-auto">
+        <ul className=" nav-ul grid grid-cols-4   full">
+          <div className="place-self-center">
+            <NavLink
+              to="/"
+              className="ml-2 font-bold text-lg lg:text-xl cursor-pointer text-center"
+            >
+              POORBOY STOCKS TRENDS
+            </NavLink>
+          </div>
+          <div className="col-span-2  place-self-center">
+            <NavLink
+              to="/stocks"
+              className=" cursor-pointer "
+              activeClassName="nav-active"
+            >
+              Stocks
+            </NavLink>
+            <NavLink
+              to="/forex"
+              className="ml-2 md:ml-6 cursor-pointer "
+              activeClassName="nav-active"
+            >
+              Forex
+            </NavLink>
+            <NavLink
+              to="/crypto"
+              className="ml-2 md:ml-6 cursor-pointer"
+              activeClassName="nav-active"
+            >
+              Crypto
+            </NavLink>
+            {/*      <NavLink
             to="/"
             className="ml-2 md:ml-6 cursor-pointer"
             activeClassName="nav-active"
@@ -46,31 +47,32 @@ export const Navbar = () => {
           <NavLink to="/" className="ml-2 md:ml-6 cursor-pointer">
             Commodities
           </NavLink> */}
-          <NavLink to="/profile" className="ml-2 md:ml-6 cursor-pointer">
-            Profile
-          </NavLink>
-          <NavLink to="/about-us" className="ml-2 md:ml-6 cursor-pointer">
-            About Us
-          </NavLink>
-        </div>
+            <NavLink to="/profile" className="ml-2 md:ml-6 cursor-pointer">
+              Profile
+            </NavLink>
+            <NavLink to="/about-us" className="ml-2 md:ml-6 cursor-pointer">
+              About Us
+            </NavLink>
+          </div>
 
-        <div>
-          <NavLink
-            to="/login"
-            className=" cursor-pointer"
-            activeClassName="nav-active"
-          >
-            Login
-          </NavLink>
-          <NavLink
-            to="/register"
-            className="ml-2 md:ml-6 cursor-pointer"
-            activeClassName="nav-active"
-          >
-            Register
-          </NavLink>
-        </div>
-      </ul>
-    </nav>
+          <div className="place-self-center">
+            <NavLink
+              to="/login"
+              className=" cursor-pointer"
+              activeClassName="nav-active"
+            >
+              Login
+            </NavLink>
+            <NavLink
+              to="/register"
+              className="ml-2 md:ml-6 cursor-pointer"
+              activeClassName="nav-active"
+            >
+              Register
+            </NavLink>
+          </div>
+        </ul>
+      </nav>
+    </>
   );
 };
