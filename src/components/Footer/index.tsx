d0e52dc8d1a35f583bc6664008430fca9cfa80de
David <drizzzydave@gmail.com>
2021-10-07 01:09:34 +0100

    fixed footer responsiveness

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,12 +1,12 @@
 export const Footer = () => {
   return (
     <footer>
-      <ul className="flex justify-center pb-8 ">
+      <ul className="flex justify-center pb-8 flex-wrap text-sm sm:text-base px-2 ">
         <li>About</li>
-        <li className="pl-10">Advertise</li>
-        <li className="pl-10">Privacy</li>
-        <li className="pl-10">Contact</li>
-        <li className="pl-10">Help</li>
+        <li className=" pl-3 md:pl-10">Advertise</li>
+        <li className="pl-3  md:pl-10">Privacy</li>
+        <li className="pl-3  md:pl-10">Contact</li>
+        <li className="pl-3  md:pl-10">Help</li>
       </ul>
     </footer>
   );
