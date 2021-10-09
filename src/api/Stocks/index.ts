3cd0a44f6c52b183a1b85c12c37fa41d005976b7
David <drizzzydave@gmail.com>
2021-10-09 14:05:28 +0100

    api function for getting Stock Chart info

--- src/api/Stocks/index.ts
+++ src/api/Stocks/index.ts
@@ -12,6 +12,21 @@ export const getStockInfo = async (symbol: string) => {
   }
 };
 
+export const getStockChartInfo = async (
+  symbol: string,
+  startDate: string,
+  endDate: string
+) => {
+  try {
+    const res = await axios.get(
+      `${twelveDataUrl}/time_series?symbol=${symbol}&interval=1day&previous_close=True&start_date=${startDate}2&end_date=${endDate}&apikey=ad0616c2df184d679b0b9ff1833456c1`
+    );
+    return res.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 export const getCryptoInfo = async (cryptoSymbol: string) => {
   try {
     const res = await axios.get(
