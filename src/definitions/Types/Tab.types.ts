b13c53062454c99f0de74e633585402c865444e9
David <drizzzydave@gmail.com>
2021-10-16 18:10:33 +0100

    added Tab Types file

---
+++ src/definitions/Types/Tab.types.ts
@@ -0,0 +1,8 @@
+export enum TabTypes {
+  performance = "performance",
+  dailyMatchTrend = "dailyMatchTrend",
+  correlation = "correlation",
+  sameMonthCorrelation = "sameMonthCorrelation",
+  linearRegression = "linearRegression",
+  equations = "equation",
+}
